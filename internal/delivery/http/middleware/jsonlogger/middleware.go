4f06875184631273da8a1a3fa8511116347d0876
Roman Kravchuk <kravchuk.roman02@gmail.com>
2024-01-02 23:56:46 +0800

    feat: json middleware

--- internal/delivery/http/middleware/jsonlogger/middleware.go
+++ internal/delivery/http/middleware/jsonlogger/middleware.go
@@ -1,1 +1,45 @@
 package jsonlogger
+
+import (
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"time"
+)
+
+type wrappedResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+	size       int
+}
+
+func (w *wrappedResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *wrappedResponseWriter) Write(b []byte) (int, error) {
+	size, err := w.ResponseWriter.Write(b)
+	w.size += size
+	return size, err
+}
+
+func New(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		ww := &wrappedResponseWriter{ResponseWriter: w}
+
+		start := time.Now()
+		defer func() {
+			logrus.WithFields(logrus.Fields{
+				"uri":      r.RequestURI,
+				"method":   r.Method,
+				"status":   ww.statusCode,
+				"size":     ww.size,
+				"duration": time.Since(start).String(),
+			}).Info("request completed")
+		}()
+
+		next.ServeHTTP(ww, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
