5f8009a3d93dc8aec67c9fab9c50df0d2158b97e
Roman Kravchuk <kravchuk.roman02@gmail.com>
2024-01-02 23:56:25 +0800

    wip: account handlers

--- internal/delivery/http/controller/account/controller.go
+++ internal/delivery/http/controller/account/controller.go
@@ -1,1 +1,58 @@
 package account
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/insan1a/tech-tinker/internal/lib/response"
+	"net/http"
+)
+
+type controller struct{}
+
+func MountRoutes(router chi.Router) {
+	c := &controller{}
+
+	router.Route("/account", func(r chi.Router) {
+		r.Get("/", c.HandleAccountInfo)
+		r.Route("/orders", func(r chi.Router) {
+			r.Get("/", c.HandleAccountOrders)
+			r.Get("/{orderID}", c.HandleAccountOrder)
+		})
+		r.Post("/statistics", c.HandleAccountStatistic)
+	})
+}
+
+func (c *controller) HandleAccountInfo(w http.ResponseWriter, r *http.Request) {
+	response.JSON(w, http.StatusOK, response.M{
+		"success": true,
+		"data": response.M{
+			"account": response.M{},
+		},
+	})
+}
+
+func (c *controller) HandleAccountOrders(w http.ResponseWriter, r *http.Request) {
+	response.JSON(w, http.StatusOK, response.M{
+		"success": true,
+		"data": response.M{
+			"orders": []response.M{},
+		},
+	})
+}
+
+func (c *controller) HandleAccountOrder(w http.ResponseWriter, r *http.Request) {
+	response.JSON(w, http.StatusOK, response.M{
+		"success": true,
+		"data": response.M{
+			"order": response.M{},
+		},
+	})
+}
+
+func (c *controller) HandleAccountStatistic(w http.ResponseWriter, r *http.Request) {
+	response.JSON(w, http.StatusOK, response.M{
+		"success": true,
+		"data": response.M{
+			"statistic": response.M{},
+		},
+	})
+}
