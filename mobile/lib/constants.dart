b172734bead399f67bee26aa7a1e9cee6251981e
Roman Kravchuk <kravchuk.roman02@gmail.com>
2024-01-03 15:44:55 +0800

    feat: backend API urls

---
+++ mobile/lib/constants.dart
@@ -0,0 +1,7 @@
+// Application
+const appName = "Tech Tinker";
+// API endpoints
+const apiUrl = "10.0.2.2:3000/account";
+const profileUrl = "$apiUrl/";
+const ordersUrl = "$apiUrl/orders";
+const statsUrl = "$apiUrl/statistics";
