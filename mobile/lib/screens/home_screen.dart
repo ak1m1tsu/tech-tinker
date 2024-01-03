934f34456c7c0571016a87a90744c2a4552f2abf
Roman Kravchuk <kravchuk.roman02@gmail.com>
2024-01-03 15:46:03 +0800

    wip: home screen

---
+++ mobile/lib/screens/home_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tech_tinker/screens/profile_screen.dart';
+import 'package:tech_tinker/screens/settings_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgets = <Widget>[
+    Scaffold(
+      body: Center(
+        child: Text("orders"),
+      ),
+    ),
+    Scaffold(
+      body: Center(
+        child: Text("statistics"),
+      ),
+    ),
+    ProfileScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgets,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 32,
+        showSelectedLabels: false,
+        unselectedItemColor: CupertinoColors.systemGrey,
+        selectedItemColor: CupertinoColors.activeBlue,
+        selectedIconTheme: const IconThemeData(
+          color: CupertinoColors.activeBlue,
+          size: 36,
+        ),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.news),
+            label: "Orders",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.chart_pie),
+            label: "Statistics",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person),
+            label: "Profile",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.gear),
+            label: "Settings",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+      ),
+    );
+  }
+
+  void _onItemTap(int value) {
+    setState(() {
+      _selectedIndex = value;
+    });
+  }
+}
